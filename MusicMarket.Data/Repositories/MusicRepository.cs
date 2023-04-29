bc5878abc0c24c04167e6aa5b52267f06e05156c
Yusa Tosun <yusatosun.yt@gmail.com>
2023-04-29 20:18:52 +0300

    Music Repo added

---
+++ MusicMarket.Data/Repositories/MusicRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicMarket.Core.Models;
+using MusicMarket.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Data.Repositories
+{
+    public class MusicRepository : Repository<Music>, IMusicRepository
+    {
+        public MusicRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Music>> GetAllWithArtistAsync(int id)
+        {
+            return await dbContext.Set<Music>().Include(m => m.Artist).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Music>> GetAllWithArtistByArtistIdAsync(int artistId)
+        {
+            return await dbContext.Set<Music>().Include(m => m.Artist).Where(m => m.ArtistId.Equals(artistId)).ToListAsync();
+        }
+
+        public async Task<Music> GetWithArtistByIdAsync(int id)
+        {
+            return await dbContext.Set<Music>().FindAsync(id);
+        }
+        private MusicMarketDBContext dbContext
+        {
+            get { return context as MusicMarketDBContext; }
+        }
+    }
+}
