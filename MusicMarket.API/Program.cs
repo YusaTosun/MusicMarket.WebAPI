8f4949cd32cd1fa0944d9c49bf8419bc8f23f0e3
Yusa Tosun <yusatosun.yt@gmail.com>
2023-04-30 00:02:13 +0300

    UnitOfWork has been completed and first migration

--- MusicMarket.API/Program.cs
+++ MusicMarket.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using MusicMarket.Core;
+using MusicMarket.Data;
+
 namespace MusicMarket.API
 {
     public class Program
@@ -7,6 +11,12 @@ namespace MusicMarket.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<MusicMarketDBContext>(opt=>
+            {
+                opt.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"),x=>x.MigrationsAssembly("MusicMarket.Data"));
+            });
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ MusicMarket.Data/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using MusicMarket.Core;
+using MusicMarket.Core.Repositories;
+using MusicMarket.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MusicMarketDBContext _context;
+        private IMusicRepository _musicRepository;
+        private IArtistRepository _artistRepository;
+        public UnitOfWork(MusicMarketDBContext context)
+        {
+            _context = context;
+        }
+        public IMusicRepository Musics => _musicRepository = _musicRepository ?? new MusicRepository(_context);
+        public IArtistRepository Artists =>_artistRepository =_artistRepository ?? new ArtistRepository(_context);
+
+        public async Task<int> CommitAsync()
+        {
+           return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
