e32f74ff8e93b395626b77d9fec1fa571dd2ac4a
Yusa Tosun <yusatosun.yt@gmail.com>
2023-05-01 00:13:39 +0300

    Services are ready to use

--- MusicMarket.API/Controllers/MusicController.cs
+++ MusicMarket.API/Controllers/MusicController.cs
@@ -49,8 +49,48 @@ namespace MusicMarket.API.Controllers
             var musicToCreate = _mapper.Map<SaveMusicDTO, Music>(saveMusicRes);
             var newMusic = await _musicService.CreateMusic(musicToCreate);
             var music = await _musicService.GetMusicById(newMusic.Id);
-            var musicRes = _mapper.Map<Music,MusicDTO>(music);
+            var musicRes = _mapper.Map<Music, MusicDTO>(music);
             return Ok(musicRes);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMusic(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var music = await _musicService.GetMusicById(id);
+            if (music == null)
+            {
+                return NotFound();
+            }
+            await _musicService.DeleteMusic(music);
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<ActionResult<MusicDTO>> Update(int id,SaveMusicDTO saveMusicRes)
+        {
+            var validator = new SaveMusicResourceValidator();
+            var validateResult = await validator.ValidateAsync(saveMusicRes);
+            if (!validateResult.IsValid)
+            {
+             return BadRequest(validateResult.Errors);
+            }
+
+            var musicToUpdate = await _musicService.GetMusicById(id);
+
+            if (musicToUpdate==null)
+            {
+                return NotFound();
+            }
+            var music = _mapper.Map<SaveMusicDTO,Music>(saveMusicRes);
+
+            await _musicService.UpdateMusic(musicToUpdate,music);
+
+            var updatedMusic = await _musicService.GetMusicById(id);
+            var updatedMusicRes = _mapper.Map<Music,MusicDTO>(updatedMusic);
+
+            return Ok(updatedMusicRes);
         }
     }
 }
